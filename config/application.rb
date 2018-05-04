27ec40e7989c51fbdc0bd133290642f991ab7bb6
Murilo Capanema <murilo.capanema@gmail.com>
2018-05-04 16:39:13 -0300

    add /lib to autoload

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module RailsRepository
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
   end
 end
